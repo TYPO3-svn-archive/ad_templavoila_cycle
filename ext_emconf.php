4348cf56c99f15784c5b70fa3a6bf791dfaa123c
adgrafik <adgrafik@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-03 16:19:14 +0000

    Bugfix release.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ad_templavoila_cycle/trunk@44577 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ad_templavoila_cycle".
 #
-# Auto generated 12-02-2011 15:21
+# Auto generated 03-03-2011 17:18
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This Extension extends Templavoila Framework with the jQuery Cycle Plugin (http://jquery.malsup.com/cycle/). NOTE: Tested only for templavoila_framework! But should work with templavoila also. Please test and response ;)',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'dependencies' => 'templavoila',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:9:"ChangeLog";s:4:"0e61";s:41:"class.tx_adtemplavoilacycle_t3dimport.php";s:4:"eed1";s:21:"ext_conf_template.txt";s:4:"3342";s:12:"ext_icon.gif";s:4:"ecd9";s:17:"ext_localconf.php";s:4:"ba43";s:14:"ext_tables.php";s:4:"6c95";s:14:"fec_import.t3d";s:4:"05a6";s:50:"core_templates/datastructures/fce/cycle (fce).html";s:4:"1c44";s:49:"core_templates/datastructures/fce/cycle (fce).xml";s:4:"cdbc";s:33:"core_templates/fce/fce_cycle.html";s:4:"3598";s:39:"core_templates/typoscript/constants.txt";s:4:"4b64";s:35:"core_templates/typoscript/setup.txt";s:4:"0dcd";s:14:"doc/manual.sxw";s:4:"29c7";s:18:"lang/locallang.xml";s:4:"5406";s:22:"lang/locallang_csh.xml";s:4:"8ab7";s:18:"res/css/simple.css";s:4:"b8b2";s:32:"res/js/jquery.cycle/example.html";s:4:"ba64";s:43:"res/js/jquery.cycle/jquery.cycle.all.min.js";s:4:"551b";s:44:"res/js/jquery.cycle/jquery.cycle.lite.min.js";s:4:"0c4a";s:39:"res/js/jquery.cycle/jquery.cycle.min.js";s:4:"d41e";s:30:"res/js/jquery.cycle/readme.txt";s:4:"6182";s:43:"res/js/jquery.cycle/src/jquery.cycle.all.js";s:4:"be03";s:39:"res/js/jquery.cycle/src/jquery.cycle.js";s:4:"2b1d";s:44:"res/js/jquery.cycle/src/jquery.cycle.lite.js";s:4:"e04c";}',
+	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"76f7";s:41:"class.tx_adtemplavoilacycle_t3dimport.php";s:4:"eed1";s:40:"class.tx_adtemplavoilatabs_t3dimport.php";s:4:"d7c9";s:21:"ext_conf_template.txt";s:4:"3342";s:12:"ext_icon.gif";s:4:"ecd9";s:17:"ext_localconf.php";s:4:"ba43";s:14:"ext_tables.php";s:4:"6c95";s:14:"fec_import.t3d";s:4:"05a6";s:50:"core_templates/datastructures/fce/cycle (fce).html";s:4:"1c44";s:49:"core_templates/datastructures/fce/cycle (fce).xml";s:4:"cdbc";s:33:"core_templates/fce/fce_cycle.html";s:4:"3598";s:39:"core_templates/typoscript/constants.txt";s:4:"4b64";s:35:"core_templates/typoscript/setup.txt";s:4:"3559";s:14:"doc/manual.sxw";s:4:"2ad1";s:18:"lang/locallang.xml";s:4:"5406";s:22:"lang/locallang_csh.xml";s:4:"8ab7";s:18:"res/css/simple.css";s:4:"b8b2";s:32:"res/js/jquery.cycle/example.html";s:4:"ba64";s:43:"res/js/jquery.cycle/jquery.cycle.all.min.js";s:4:"551b";s:44:"res/js/jquery.cycle/jquery.cycle.lite.min.js";s:4:"0c4a";s:39:"res/js/jquery.cycle/jquery.cycle.min.js";s:4:"d41e";s:30:"res/js/jquery.cycle/readme.txt";s:4:"6182";s:43:"res/js/jquery.cycle/src/jquery.cycle.all.js";s:4:"be03";s:39:"res/js/jquery.cycle/src/jquery.cycle.js";s:4:"2b1d";s:44:"res/js/jquery.cycle/src/jquery.cycle.lite.js";s:4:"e04c";}',
 	'suggests' => array(
 	),
 );
